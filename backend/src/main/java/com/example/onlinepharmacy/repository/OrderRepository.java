b27a986c5d500d5f01d03c24ae3953d85304a853
Kartik Patil <152792829+KartikpatilKP@users.noreply.github.com>
2025-08-30 20:10:27 +0530

    Delete backend directory

--- backend/src/main/java/com/example/onlinepharmacy/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlinepharmacy.repository;
-
-import com.example.onlinepharmacy.entity.DrugOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<DrugOrder, Long> {
-}
