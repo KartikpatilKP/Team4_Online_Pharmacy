c0941538f50cc4de7c496a96eb342340ab4a601b
M Divya Lalitha <mdivyalalitha29@gmail.com>
2025-08-31 00:10:12 +0530

    Delete backend directory

--- backend/src/main/java/com/example/onlinepharmacy/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.onlinepharmacy.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/example/onlinepharmacy/repository/DrugRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlinepharmacy.repository;
-
-import com.example.onlinepharmacy.entity.Drug;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrugRepository extends JpaRepository<Drug, Long> {
-}
